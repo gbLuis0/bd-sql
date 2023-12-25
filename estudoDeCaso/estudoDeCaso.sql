9fd9ac8ce83cfa2f6f4c11a8eb2fbf0b9a9fa777
Gabriel Luis <87285320+gbLuis0@users.noreply.github.com>
2023-12-25 13:35:26 -0300

    Create estudoDeCaso.sql

---
+++ estudoDeCaso/estudoDeCaso.sql
@@ -0,0 +1,116 @@
+CREATE DATABASE AgroBomDB;
+USE AgroBomDB;
+
+CREATE TABLE Cliente (
+    numeroCliente INT NOT NULL,
+    nome VARCHAR(255),
+    telefone VARCHAR(20),
+    endereco VARCHAR(255),
+    novoCliente BOOLEAN,
+     
+    CONSTRAINT pk_cliente PRIMARY KEY (numeroCliente)
+);
+
+CREATE TABLE Produto (
+    numeroProduto INT NOT NULL,
+    descricao VARCHAR(255),
+    quantidadeEstoque INT,
+    unidadeMedida VARCHAR(20),
+    qtdMinIdealEstoque INT,
+    
+    CONSTRAINT pk_produto PRIMARY KEY (numeroProduto)
+);
+
+CREATE TABLE Pedido (
+    numeroPedido INT NOT NULL,
+    dataPedido DATE,
+    valorTotal Float,
+    desconto Float,
+    numeroCliente INT,
+    
+    CONSTRAINT pk_Pedido PRIMARY KEY (numeroPedido),
+    
+    CONSTRAINT fk_Cliente FOREIGN KEY (numeroCliente)
+    REFERENCES Cliente(numeroCliente)
+    ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE PedidoProduto (
+    numeroPedido INT,
+    numeroProduto INT,
+    
+    CONSTRAINT fk_Pedido_Pedido FOREIGN KEY (numeroPedido) 
+    REFERENCES Pedido(numeroPedido)
+    ON UPDATE CASCADE ON DELETE CASCADE,
+    
+    CONSTRAINT fk_Produto_Produto FOREIGN KEY (numeroProduto) 
+    REFERENCES Produto(numeroProduto) 
+    ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE Fornecedor (
+    CNPJFornecedor VARCHAR(20) PRIMARY KEY,
+    nome VARCHAR(255),
+    telefone VARCHAR(20)
+);
+
+CREATE TABLE ProdutoFornecedor (
+    numeroProduto INT,
+    CNPJFornecedor VARCHAR(20),
+    PRIMARY KEY (numeroProduto, CNPJFornecedor),
+    FOREIGN KEY (numeroProduto) REFERENCES Produto(numeroProduto),
+    FOREIGN KEY (CNPJFornecedor) REFERENCES Fornecedor(CNPJFornecedor)
+);
+
+CREATE TABLE SolicitacaoCompra (
+    numeroSolicitacao INT PRIMARY KEY,
+    situacao VARCHAR(20),
+    CNPJFornecedor VARCHAR(20),
+    FOREIGN KEY (CNPJFornecedor) REFERENCES Fornecedor(CNPJFornecedor)
+);
+
+
+INSERT INTO Cliente
+(numeroCliente, nome, telefone, endereco, novoCliente)
+VALUES
+(1, 'Joaquim Ferreira', '123-456-7890', 'Rua Alto, 123', FALSE),
+(2, 'Mario Oliveira', '987-654-3210', 'Avenida Calhau, 456', TRUE);
+
+INSERT INTO PedidoProduto
+(numeroPedido, numeroProduto)
+VALUES
+(101, 1),
+(101, 2),
+(102, 3);
+
+INSERT INTO Pedido
+(numeroPedido, dataPedido, valorTotal, desconto, numeroCliente)
+VALUES
+(101, '2023-12-18', 500.00, 20.00, 1),
+(102, '2023-12-19', 300.00, 10.00, 2);
+       
+INSERT INTO Produto
+(numeroProduto, descricao, quantidadeEstoque, unidadeMedida, qtdMinIdealEstoque)
+VALUES
+(1, 'Produto X', 50, 'Unidade', 10),
+(2, 'Produto Y', 30, 'Kg', 5),
+(3, 'Produto Z', 20, 'Litro', 8);
+
+INSERT INTO Fornecedor
+(CNPJFornecedor, nome, telefone)
+VALUES
+('12345678901234', 'Fornecedor XYZ', '111-222-3333'),
+('56789012345678', 'Fornecedor ABC', '444-555-6666');
+
+INSERT INTO ProdutoFornecedor
+(numeroProduto, CNPJFornecedor)
+VALUES
+(1, '12345678901234'),
+(2, '12345678901234'),
+(3, '56789012345678');
+
+INSERT INTO SolicitacaoCompra
+(numeroSolicitacao, situacao, CNPJFornecedor)
+VALUES
+(201, 'Em aberto', '12345678901234'),
+(202, 'Encerrada', '56789012345678');
